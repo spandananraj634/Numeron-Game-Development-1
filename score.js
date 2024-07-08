ad0598bc676350d5bd571222687045ef937c2fc5
spandananraj634 <83412339+spandananraj634@users.noreply.github.com>
2024-07-09 00:13:04 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const playAgainBtn = document.getElementById("play-again-button");
+const scoreBoard = document.getElementById("score-board");
+
+if (playAgainBtn)
+  playAgainBtn.onclick = function () {
+    window.location.href = "./index.html";
+  };
+if (scoreBoard) scoreBoard.innerText = localStorage.getItem("score");
